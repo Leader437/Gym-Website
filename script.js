143d31722ed2071509b73bec0c77b8b58e5c1501
Leader <129864685+Leader437@users.noreply.github.com>
2023-06-22 11:17:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+
+// hamburger
+let navBtn = document.querySelector('.hamburger');
+let navbar = document.querySelector('nav');
+
+navBtn.addEventListener('click', () => {
+    navBtn.classList.toggle('cross');
+    navbar.classList.toggle('open');
+});
+
+
+// slider
+const swiper = new Swiper('.feedback', {
+    autoplay: {
+        delay: 2000,
+        disableonInteraction: false,
+    },
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 25,
+    freeMode: 'true',
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: true,
+    
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    }
+});
+
+
+// FAQ'S
+let faqItem = document.querySelector('.faq-item');
+let faqBtn = document.querySelectorAll('.faq-btn');
+
+faqBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        faqItem.classList.toggle('faq-open');
+    }); 
+});
